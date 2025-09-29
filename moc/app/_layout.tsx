8ab315fdb2232ec9a433d6faa845783b74bbcd8e
srinivas-01 <62439147+srinivas-01@users.noreply.github.com>
2025-09-29 14:39:09 +0530

    test 14

--- moc/app/_layout.tsx
+++ moc/app/_layout.tsx
@@ -4,6 +4,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -14,20 +15,22 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="screens/LoginScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="screens/OtpScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="screens/CameraScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/NewListScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/ViewListScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/PreviewScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/MocScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/ListsScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/LinkListScreen" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="screens/SelectedPreview" options={{ presentation: 'fullScreenModal' }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <SafeAreaProvider>
+        <Stack initialRouteName="screens/LoginScreen" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="screens/LoginScreen" options={{ headerShown: false }} />
+          <Stack.Screen name="screens/OtpScreen" options={{ headerShown: false }} />
+          <Stack.Screen name="screens/CameraScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/NewListScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/ViewListScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/PreviewScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/MocScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/ListsScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/LinkListScreen" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="screens/SelectedPreview" options={{ presentation: 'fullScreenModal' }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SafeAreaProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
