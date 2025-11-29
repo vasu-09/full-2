d73d4f6cf4a3a9e3b481ae6bd0d7392be5cc267e
harika gurazala <gvasunani2000@gmail.com>
2025-11-29 09:26:10 +0530

    integration 2453

--- api-gateway/src/main/java/com/om/api_gateway/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/om/api_gateway/GatewaySecurityConfig.java
@@ -2,23 +2,34 @@ package com.om.api_gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+
 
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.util.StringUtils;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
 @EnableWebFluxSecurity
 public class GatewaySecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    @Order(1)
+    SecurityWebFilterChain websocketSecurity(ServerHttpSecurity http) {
+        return http
+                .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/ws", "/ws/**", "/rtc/ws", "/rtc/ws/**"))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                // Let the RTC service validate tokens during the websocket handshake
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::disable)
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityWebFilterChain apiSecurity(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(ex -> ex
